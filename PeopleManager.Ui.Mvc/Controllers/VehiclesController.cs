ca3ffa4f4b8ed27383b23c23c976746bb59687be
VandammeLaure2004 <125573527+VandammeLaure2004@users.noreply.github.com>
2023-05-23 11:24:44 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/VehiclesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly PeopleManagerDbContext _dbContext;
+
+        public VehiclesController(PeopleManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var vehicles = _dbContext.Vehicles.ToList();
+            return View(vehicles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vehicle vehicle)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vehicle);
+            }
+
+            _dbContext.Vehicles.Add(vehicle);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var vehicle = _dbContext.Vehicles.Find(id);
+
+            if (vehicle is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute]int id, [FromForm]Vehicle vehicle)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vehicle);
+            }
+
+            var dbVehicle = _dbContext.Vehicles.Find(id);
+            if (dbVehicle is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbVehicle.LicensePlate = vehicle.LicensePlate;
+            dbVehicle.Brand = vehicle.Brand;
+            dbVehicle.Type = vehicle.Type;
+            dbVehicle.ResponsiblePersonId = vehicle.ResponsiblePersonId;
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var vehicle = _dbContext.Vehicles.Find(id);
+
+            if (vehicle is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        [HttpPost("Vehicles/Delete/{id:int?}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            //var vehicle = _dbContext.Vehicles.Find(id);
+
+            //if (vehicle is null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+            var vehicle = new Vehicle
+            {
+                Id = id,
+                LicensePlate = string.Empty
+            };
+            _dbContext.Vehicles.Attach(vehicle);
+
+            _dbContext.Vehicles.Remove(vehicle);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/Core/PeopleManagerDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Core
+{
+    public class PeopleManagerDbContext : DbContext
+    {
+
+        public PeopleManagerDbContext(DbContextOptions<PeopleManagerDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>()
+                .HasOne(v => v.ResponsiblePerson)
+                .WithMany(p => p.ResponsibleForVehicles)
+                .HasForeignKey(v => v.ResponsiblePersonId)
+                .IsRequired(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void Seed()
+        {
+            People.AddRange(new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "Bavo",
+                    LastName = "Ketels",
+                    Email = "bavo.ketels@vives.be",
+                    Description = "Lector"
+                },
+                new Person{FirstName = "Isabelle", LastName = "Vandoorne", Email = "isabelle.vandoorne@vives.be" },
+                new Person
+                {
+                    FirstName = "Wim",
+                    LastName = "Engelen",
+                    Email = "wim.engelen@vives.be",
+                    Description = "Opleidingshoofd"
+                },
+                new Person{FirstName = "Ebe", LastName = "Deketelaere", Email = "ebe.deketelaere@vives.be" }
+            });
+
+            Vehicles.AddRange(new[] {
+                new Vehicle{LicensePlate = "1-ABC-123"},
+                new Vehicle{LicensePlate = "THE_BOSS", Brand= "Ferrari", Type="448"},
+                new Vehicle{LicensePlate = "SALES_GUY_1", Brand= "Audi", Type="e-tron"},
+                new Vehicle{LicensePlate = "DESK_1", Brand= "Fiat", Type="Punto"}});
+
+            SaveChanges();
+        }
+    }
+}
